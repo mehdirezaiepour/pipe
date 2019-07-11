bdbad55c07274ac58a149045cd30b304c78ef7f9
Mehdi Rezaiepour <mehdi.rezaiepour@fau.de>
2019-07-11 19:57:19 +0200

    nitial commit
     Changes to be committed:
    	new file:   CMakeLists.txt
    	new file:   main.cpp
    	new file:   pipe_sever.cpp
    	new file:   pipe_sever.h

---
+++ pipe_sever.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mehdi on 7/11/19.
+//
+
+#include "pipe_sever.h"
---
+++ pipe_sever.h
@@ -0,0 +1,15 @@
+//
+// Created by mehdi on 7/11/19.
+//
+
+#ifndef NAMED_PIPE_PIPE_SEVER_H
+#define NAMED_PIPE_PIPE_SEVER_H
+
+
+class pipe_sever
+{
+
+};
+
+
+#endif //NAMED_PIPE_PIPE_SEVER_H
